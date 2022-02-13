20a27b1334ec4466da20ab36ffce77907649316b
Patryk <patryk.bogdanski95@gmail.com>
2022-02-13 18:45:38 +0100

    Commit

--- MagadiApp.Services.ProductAPI/Controllers/WeatherForecastController.cs
+++ MagadiApp.Services.ProductAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace MagadiApp.Services.ProductAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Freezing"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
