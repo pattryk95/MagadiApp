883f451029d731e7b290622a0ce55a31563a28f1
Patryk <patryk.bogdanski95@gmail.com>
2022-02-19 13:02:57 +0100

    Implement product service

--- MagadiApp.Web/Services/BaseService.cs
+++ MagadiApp.Web/Services/BaseService.cs
@@ -18,7 +18,7 @@ namespace MagadiApp.Web.Services
             this.httpClient = httpClient;
         }
 
-        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
         {
             try
             {
@@ -37,21 +37,38 @@ namespace MagadiApp.Web.Services
                 switch (apiRequest.ApiType)
                 {
                     case SD.ApiType.GET:
+                        message.Method = HttpMethod.Get;
                         break;
                     case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
                         break;
                     case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
                         break;
                     case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
                         break;
                     default:
+                        message.Method = HttpMethod.Get;
                         break;
                 }
+                apiResponse = await client.SendAsync(message);
+
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(apiContent);
+                return apiResponseDto;
             }
             catch (Exception e)
             {
-
-                throw;
+                var dto = new ResponseDto
+                {
+                    DisplayMessage = "Error",
+                    ErrorMessages = new List<string> { Convert.ToString(e.Message) },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(res);
+                return apiResponseDto;
             }
         }
         public void Dispose()
---
+++ MagadiApp.Web/Services/ProductService.cs
@@ -0,0 +1,71 @@
+﻿using MagadiApp.Web.Models;
+using MagadiApp.Web.Models.Dtos;
+using MagadiApp.Web.Services.IServices;
+
+namespace MagadiApp.Web.Services
+{
+    public class ProductService : BaseService, IProductService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public ProductService(IHttpClientFactory clientFactory) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+        public async Task<T> CreateProductAsync<T>(ProductDto productDto)
+        {
+            return await SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = productDto,
+                Url = SD.ProductAPIBase + "/api/products",
+                AccessToken = ""
+
+            });   
+        }
+
+        public async Task<T> DeleteProductAsync<T>(int id)
+        {
+            return await SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.DELETE,
+                Url = SD.ProductAPIBase + "/api/products" + id,
+                AccessToken = ""
+
+            });
+        }
+
+        public async Task<T> GetAllProductsAsync<T>()   
+        {
+            return await SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "/api/products",
+                AccessToken = ""
+
+            });
+        }
+
+        public async Task<T> GetProductByIdAsync<T>(int id)
+        {
+            return await SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "/api/products" + id,
+                AccessToken = ""
+
+            });
+        }
+
+        public async Task<T> UpdateProductAsync<T>(ProductDto productDto)
+        {
+            return await SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.PUT,
+                Url = SD.ProductAPIBase + "/api/products",
+                AccessToken = ""
+
+            });
+        }
+    }
+}
