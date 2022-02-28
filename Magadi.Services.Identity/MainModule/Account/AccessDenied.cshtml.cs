9d1a43df8cdedc0d8ae771ec1733e90671fadc10
Patryk <patryk.bogdanski95@gmail.com>
2022-02-28 20:38:24 +0100

    Create Identity Project and make DbContext for identity

---
+++ Magadi.Services.Identity/MainModule/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MagadiApp.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ Magadi.Services.Identity/MainModule/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,8 @@
+
+namespace MagadiApp.Pages.Logout;
+
+public class LogoutOptions
+{
+    public static bool ShowLogoutPrompt = true;
+    public static bool AutomaticRedirectAfterSignOut = false;
+}
