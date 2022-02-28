1e7c7e15acc11f3c6261ad5b86d68aa729d0e398
Patryk <patryk.bogdanski95@gmail.com>
2022-02-28 12:20:10 +0100

    Change ProductDTO to be optional

--- MagadiApp.Web/Models/ProductDto.cs
+++ MagadiApp.Web/Models/ProductDto.cs
@@ -5,7 +5,7 @@
         public int ProductId { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public CategoryDto? CategoryDto { get; set; }
     }
 }
